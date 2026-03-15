e7c4f92b1d5ffd1a839c834522501e0455997d99
Leonardo Lorenzano <losl060528@gs.utm.mx>
2026-03-14 19:48:16 -0600

    funcion ASM

--- division.asm
+++ division.asm
@@ -3,17 +3,17 @@ section .text
 
 division: 
     ; RDI es el divisor 
-    ; RSI es el divividendo 
+    ; RSI es el dividendo 
 
-    ; Vlidacion de division entre cero
-    cmp rsi, 0
+    ; Vlidacion: division entre cero
+    cmp rsi, 0                      ; ¿El dividendo es cero? 
     je .es_cero
 
     mov rax, rdi 
-    cqo                      ; Extendemos el signo
-    idiv rsi                 ; Hacemos la dividion de RDI entre RSI 
-    ret                      ; Retornamos el resultado en rax
+    cqo                             ; Extendemos el signo
+    idiv rsi                        ; Hacemos la division de RDI entre RSI 
+    ret 
 
 .es_cero: 
-    mov rax, 0               ; Retornamos cero si el divisor es cero
-    ret
+    xor rax, rax                    ; Retornamos el valor de '0' 
+    ret 
